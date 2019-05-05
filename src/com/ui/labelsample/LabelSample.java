89f9e570f631894bfce7ca375973d7edcde5b577
tony <fengfeng043@gmail.com>
2019-05-05 18:44:56 +0800

    添加注释

--- src/com/ui/labelsample/LabelSample.java
+++ src/com/ui/labelsample/LabelSample.java
@@ -47,7 +47,7 @@ import javafx.stage.Stage;
 
 public class LabelSample extends Application {
 
-Label label3 = new Label("A label that needs to be wrapped");
+    Label label3 = new Label("A label that needs to be wrapped");
 
     public static void main(String[] args) {
         launch(args);
@@ -59,7 +59,7 @@ Label label3 = new Label("A label that needs to be wrapped");
         stage.setTitle("Label Sample");
         stage.setWidth(420);
         stage.setHeight(180);
-        
+
 
         HBox hbox = new HBox();
         Image image = new Image(getClass().getResourceAsStream("labels.jpg"));
@@ -68,12 +68,13 @@ Label label3 = new Label("A label that needs to be wrapped");
         label1.setGraphic(new ImageView(image));
         label1.setFont(new Font("Arial", 30));
         label1.setTextFill(Color.web("#0076a3"));
-        label1.setTextAlignment(TextAlignment.JUSTIFY);      
-    
-        Label label2 = new Label ("Values");
+        label1.setTextAlignment(TextAlignment.JUSTIFY);// 排成直线
+
+
+        Label label2 = new Label("Values");
         label2.setFont(Font.font("Cambria", 32));
-        label2.setRotate(270);
-        label2.setTranslateY(50);
+        label2.setRotate(270);// 旋转
+        label2.setTranslateY(50); // 平移
 
         label3.setWrapText(true);
         label3.setTranslateY(50);
@@ -82,7 +83,7 @@ Label label3 = new Label("A label that needs to be wrapped");
         label3.setOnMouseEntered((MouseEvent e) -> {
             label3.setScaleX(1.5);
             label3.setScaleY(1.5);
-        });
+        }); // 缩放
 
         label3.setOnMouseExited((MouseEvent e) -> {
             label3.setScaleX(1);
@@ -93,7 +94,7 @@ Label label3 = new Label("A label that needs to be wrapped");
         hbox.getChildren().add((label1));
         hbox.getChildren().add(label2);
         hbox.getChildren().add(label3);
-        ((Group)scene.getRoot()).getChildren().add(hbox);
+        ((Group) scene.getRoot()).getChildren().add(hbox);
 
         stage.setScene(scene);
         stage.show();
