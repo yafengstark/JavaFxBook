104843dc0cef514893d6f05ba05def5aa1418c7a
tony <fengfeng043@gmail.com>
2019-05-06 12:38:16 +0800

    分页

---
+++ src/com/ui/paginationsample/PaginationDemo.java
@@ -0,0 +1,102 @@
+package com.ui.paginationsample;
+
+/**
+ * @author tony
+ * @date 2019/5/6 12:35
+ */
+/*
+ * Copyright (c) 2008, 2014, Oracle and/or its affiliates.
+ * All rights reserved. Use is subject to license terms.
+ *
+ * This file is available and licensed under the following license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the distribution.
+ *  - Neither the name of Oracle nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class PaginationDemo extends Application {
+
+    private Pagination pagination;
+    final String[] textPages = new String[]{
+            "this is a test 1",
+            "this is a test 2",
+            "this is a test 3",
+            "this is a test 4",
+            "this is a test 5",
+            "this is a test 6",
+            "this is a test 7",
+            "this is a test 8",
+            "this is a test 8",
+    };
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    public int itemsPerPage() {
+        return 1;
+    }
+
+    public VBox createPage(int pageIndex) {
+        VBox box = new VBox(5);
+        int page = pageIndex * itemsPerPage();
+        for (int i = page; i < page + itemsPerPage(); i++) {
+            TextArea text = new TextArea(textPages[i]);
+            text.setWrapText(true);
+            box.getChildren().add(text);
+        }
+        return box;
+    }
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        pagination = new Pagination(28, 0);
+        pagination.setPageFactory((Integer pageIndex) -> {
+            if (pageIndex >= textPages.length) {
+                return null;
+            } else {
+                return createPage(pageIndex);
+            }
+        });
+
+        AnchorPane anchor = new AnchorPane();
+        AnchorPane.setTopAnchor(pagination, 10.0);
+        AnchorPane.setRightAnchor(pagination, 10.0);
+        AnchorPane.setBottomAnchor(pagination, 10.0);
+        AnchorPane.setLeftAnchor(pagination, 10.0);
+        anchor.getChildren().addAll(pagination);
+        Scene scene = new Scene(anchor, 400, 250);
+        stage.setScene(scene);
+        stage.setTitle("PaginationSample");
+        stage.show();
+    }
+}
