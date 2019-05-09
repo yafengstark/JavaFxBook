504cf3272ae43af73b7cc9363085f417fa872355
tony <fengfeng043@gmail.com>
2019-05-09 21:49:24 +0800

    解决主线程更新UI

---
+++ src/com/ui/UpdateUi.java
@@ -0,0 +1,13 @@
+package com.ui;
+
+/**
+ *
+ * 解决 ：java.lang.IllegalStateException:
+ * Not on FX application thread; currentThread =
+ * @author tony
+ * @date 2019/5/9 21:46
+ */
+public class UpdateUi {
+
+
+}
