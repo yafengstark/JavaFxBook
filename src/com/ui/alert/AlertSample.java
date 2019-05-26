65dd0b33223be54a03dfb360038027721f112a9e
tony <fengfeng043@gmail.com>
2019-05-26 16:56:26 +0800

    提示框

--- src/com/ui/alert/AlertSample.java
+++ src/com/ui/alert/AlertSample.java
@@ -1,4 +1,28 @@
 package com.ui.alert;
 
+import javafx.scene.control.Alert;
+
 public class AlertSample {
+
+
+    public  static void alertSuccess(String content){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("成功");
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+
+        alert.showAndWait();
+
+    }
+
+    public static void alertFail(String content){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("失败");
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+
+        alert.showAndWait();
+
+    }
+
 }
